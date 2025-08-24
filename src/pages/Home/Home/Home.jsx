8d5231cfa637b48824917e8e0f08d91aca1028b2
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2025-08-25 00:36:12 +0600

    Created react router and added footer

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className='text-red-300 text-6xl'>This is Home</h2>
+            <h5 className='text-red-300'>This is what</h5>
+            
+        </div>
+    );
+};
+
+export default Home;
